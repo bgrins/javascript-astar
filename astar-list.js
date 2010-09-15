591c1b442d5f9ee160262e9a6d225a5e0260dd7d
Brian Grinstead <briangrinstead@gmail.com>
2010-09-15 13:35:10 -0500

    using binary heap instead of list.  Also a few steep performance improvements involving using bits for keeping track of visited and closed instead of searching lists. Everything is way faster (around 5ms for 100x100 on chrome vs around 700-1000ms).

--- astar-list.js
+++ astar-list.js
@@ -1,10 +1,10 @@
-/* 	astar.js http://github.com/bgrins/javascript-astar
-	Implements the astar search algorithm in javascript using a binary heap
+/* 	astar-list.js http://github.com/bgrins/javascript-astar
+	Implements the astar search algorithm in javascript
+	Based off the original blog post http://www.briangrinstead.com/blog/astar-search-algorithm-in-javascript
+	It has since been replaced with astar.js which uses a Binary Heap and is quite faster, but I am leaving
+	it here since it is more strictly following pseudocode for the Astar search
 	**Requires graph.js**
 	
-	Binary Heap taken from http://eloquentjavascript.net/appendix2.html
-	with license: http://creativecommons.org/licenses/by/3.0/
-		
 	Example Usage:
 		var graph = new Graph([
 			[0,0,0,0],
@@ -17,20 +17,18 @@
 		
 	See graph.js for a more advanced example
 */
- 
+
 var astar = {
     init: function(grid) {
         for(var x = 0; x < grid.length; x++) {
             for(var y = 0; y < grid[x].length; y++) {
-            	var node = grid[x][y];
-            	node.f
-                node.f = 0;
-                node.g = 0;
-                node.h = 0;
-                node.visited = false;
-                node.closed = false;
-                node.debug = "";
-                node.parent = null;
+                grid[x][y].f = 0;
+                grid[x][y].g = 0;
+                grid[x][y].h = 0;
+                grid[x][y].visited = false;
+                grid[x][y].closed = false;
+                grid[x][y].debug = "";
+                grid[x][y].parent = null;
             }   
         }
     },
@@ -41,14 +39,14 @@ var astar = {
         var openList   = [];
         openList.push(start);
         
+        while(openList.length > 0) {
         
-		var openHeap = new BinaryHeap(function(node){return node.f;});
-		openHeap.push(start);
-        
-        while(openHeap.size() > 0) {
-        	
-        	// Grab the lowest f(x) to process next.  Heap keeps this sorted for us.
-            var currentNode = openHeap.pop();
+        	// Grab the lowest f(x) to process next
+            var lowInd = 0;
+            for(var i=0; i<openList.length; i++) {
+			    if(openList[i].f < openList[lowInd].f) { lowInd = i; }
+		    }
+		    var currentNode = openList[lowInd];
 		    
 		    // End case -- result has been found, return the traced path
 		    if(currentNode == end) {
@@ -62,6 +60,7 @@ var astar = {
 		    }
     		
 		    // Normal case -- move currentNode from open to closed, process each of its neighbors
+		    openList.remove(lowInd);
 		    currentNode.closed = true;
 		    
 		    var neighbors = astar.neighbors(grid, currentNode);
@@ -75,29 +74,30 @@ var astar = {
     			
 			    // g score is the shortest distance from start to current node, we need to check if
 			    //   the path we have arrived at this neighbor is the shortest one we have seen yet
-			    // 1 is the distance from a node to it's neighbor.  This could be variable for weighted paths.
-			    var gScore = currentNode.g + 1; 
+			    var gScore = currentNode.g + 1; // 1 is the distance from a node to it's neighbor
 			    var gScoreIsBest = false;
-			    var beenVisited = neighbor.visited;
     			
-			    if(!beenVisited || gScore < neighbor.g) {
-			    
-				    // Found an optimal (so far) path to this node.  Take score for node to see how good it is.				    
+			    if(!neighbor.visited) {
+				    // This the the first time we have arrived at this node, it must be the best
+				    // Also, we need to take the h (heuristic) score since we haven't done so yet
+    				
+				    gScoreIsBest = true;
+				    neighbor.h = heuristic(neighbor.pos, end.pos);
 				    neighbor.visited = true;
+				    openList.push(neighbor);
+			    }
+			    else if(gScore < neighbor.g) {
+				    // We have already seen the node, but last time it had a worse g (distance from start)
+				    gScoreIsBest = true;
+			    }
+    			
+			    if(gScoreIsBest) {
+				    // Found an optimal (so far) path to this node.  Store info on how we got here and
+				    //  just how good it really is...
 				    neighbor.parent = currentNode;
-				    neighbor.h = neighbor.h || heuristic(neighbor.pos, end.pos);
 				    neighbor.g = gScore;
 				    neighbor.f = neighbor.g + neighbor.h;
 				    neighbor.debug = "F: " + neighbor.f + "<br />G: " + neighbor.g + "<br />H: " + neighbor.h;
-				    
-				    if (!beenVisited) {
-				    	// Pushing to heap will put it in proper place based on the 'f' value.
-				    	openHeap.push(neighbor);
-				    }
-				    else {
-				    	// Already seen the node, but since it has been rescored we need to reorder it in the heap
-				    	openHeap.rescoreElement(neighbor);
-				    }
 				}
 		    }
         }
