eb5a7dcd54c470ebd95015e0bd0c2221049e1f17
TiTi <anthibug@gmail.com>
2014-06-12 19:50:42 +0200

    Remove extra spaces

--- astar.js
+++ astar.js
@@ -134,15 +134,15 @@ var astar = {
     // See list of heuristics: http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
     heuristics: {
         manhattan: function(pos0, pos1) {
-            var d1 = Math.abs (pos1.x - pos0.x);
-            var d2 = Math.abs (pos1.y - pos0.y);
+            var d1 = Math.abs(pos1.x - pos0.x);
+            var d2 = Math.abs(pos1.y - pos0.y);
             return d1 + d2;
         },
         diagonal: function(pos0, pos1) {
             var D = 1;
             var D2 = Math.sqrt(2);
-            var d1 = Math.abs (pos1.x - pos0.x);
-            var d2 = Math.abs (pos1.y - pos0.y);
+            var d1 = Math.abs(pos1.x - pos0.x);
+            var d2 = Math.abs(pos1.y - pos0.y);
             return (D * (d1 + d2)) + ((D2 - (2 * D)) * Math.min(d1, d2));
         }
     }
